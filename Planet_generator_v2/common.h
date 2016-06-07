01ac95ba5776f976b82f4716d41a8ab60ecbe883
LorisAiulfi <loris.aiulfi@epfl.ch>
2016-06-07 13:36:04 +0200

    adapted common.h for laptop

--- Planet_generator_v2/common.h
+++ Planet_generator_v2/common.h
@@ -2,30 +2,32 @@
 
 // GLEW
 #define GLEW_STATIC
-#include <GL/glew.h>
+//#include <GL/glew.h>
+#include <glew.h>
 // GLFW
-#include <GLFW/glfw3.h>
+#include <glfw3.h>
 
 #include <iostream>
 #include <cassert>
 
 // Eigen is used for linear algebra
-#include <Eigen\Dense>
+#include <Dense>
 typedef Eigen::Vector2f vec2;
 typedef Eigen::Vector3f vec3;
 typedef Eigen::Vector4f vec4;
 typedef Eigen::Matrix4f mat4;
 typedef Eigen::Matrix3f mat3;
-#include <OpenGP\GL\EigenOpenGLSupport3.h>
+//#include <OpenGP\GL\EigenOpenGLSupport3.h>
+#include <EigenOpenGLSupport3.h>
 
 // Shader compiling utilities
-#include <OpenGP\GL\shader_helpers.h>
+#include <shader_helpers.h>
 
 // Utilities to simplify glfw setup
-#include <OpenGP\GL\glfw_helpers.h>
+#include <glfw_helpers.h>
 
 // For mesh I/O we use OpenGP
-#include <OpenGP\Surface_mesh.h>
+#include <Surface_mesh.h>
 
 // For GL error checking
 #include "check_gl_error.h"
